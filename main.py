291f04909f1be732e14fbfa6d946af54d9b179dc
Tudor Avram <a96tudor@gmail.com>
2018-01-30 23:59:01 +0000

    finished the skeleton for the main entry point to the project

--- main.py
+++ main.py
@@ -1,16 +1,52 @@
-import argparse
+import argparse, sys
 
 
-def build_training_set():
-    print("Training set")
+def build_training_set(args):
+    """
+        Function that takes the appropriate actions in order to build the training set
+
+    :param args:            A dictionary containing the required arguments for building the training set
+    :return:                -
+    """
+    print("Training set...")
 
 
-def train_data():
+def train_data(args):
+    """
+            Function that takes the appropriate actions in order to train the model
+
+        :param args:            A dictionary containing the required arguments for training the model
+        :return:                -
+    """
     print("Training...")
 
 
-def evaluate():
+def evaluate(args):
+    """
+            Function that takes the appropriate actions in order to evaluate the model
+
+        :param args:            A dictionary containing the required arguments for evaluating the model
+        :return:                -
+    """
     print("Evaluating...")
+
+
+def classify(args):
+    """
+                Function that takes the appropriate actions in order to classify a single node
+
+            :param args:            A dictionary containing the required arguments for classifying a single node
+            :return:                -
+    """
+    print("Classifying...")
+
+
+_COMMAND_ACTION = {
+    'get_training_set': build_training_set,
+    'train': train_data,
+    'eval': evaluate,
+    'classify': classify
+}
 
 
 def parser_setup():
@@ -21,12 +57,13 @@ def parser_setup():
     """
     parser = argparse.ArgumentParser(prog='main')
 
-    subparsers = parser.add_subparsers(help='Sub-commands help:')
+    subparsers = parser.add_subparsers(help='Sub-commands help:', dest='command')
 
 
     # Creating subparser for building training set
     parser_training_set = subparsers.add_parser('get_training_set',
-                                                help='Sub-command used to build the training set')
+                                                help='Sub-command used to build the training set'
+                                                )
 
     parser_training_set.add_argument('--host',
                                      default='127.0.0.1',
@@ -54,7 +91,8 @@ def parser_setup():
 
     # Creating the subparser for training data
     parser_train = subparsers.add_parser('train',
-                                         help='Sub-command for training the data')
+                                         help='Sub-command for training the data',
+                                        )
 
     parser_train.add_argument('--set',
                               default='data/training/training_set.csv',
@@ -63,7 +101,8 @@ def parser_setup():
 
     # Creating the subparser for evaluating a single node
     parser_classify = subparsers.add_parser('classify',
-                                             help='Sub-command for classifying a single node')
+                                             help='Sub-command for classifying a single node'
+                                            )
 
     parser_classify.add_argument('--host',
                                  default='127.0.0.1',
@@ -74,20 +113,34 @@ def parser_setup():
                                      default=7474,
                                      help='Port number where the Neo4J database runs on. Default 7474')
 
-    parser_training_set.add_argument('--node',
+    parser_classify.add_argument('--node',
                                      help='ID of the node we want to evaluate')
 
 
     # Creating the subparser for evaluating the entire model
     parser_evaluate = subparsers.add_parser('eval',
-                                            help='Sub-command for ')
+                                            help='Sub-command for evaluating the entire model',
+                                           )
+
 
     return parser
 
 
 if __name__ == '__main__':
     parser = parser_setup()
-    args = parser.parse_args()
+    args = parser.parse_args(["get_training_set"])
+    print(args)
     args = vars(args)
     if len(args) == 0:
         parser.print_help()
+        sys.exit()
+    elif len(args) == 1:
+        parser.print_help()
+        sys.exit()
+    else:
+        for key in args:
+            if args[key] is None:
+                parser.print_help()
+                sys.exit()
+
+        # Otherwise, all good, we can continue
