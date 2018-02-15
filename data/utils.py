04bc6dc04b93fe0d0cc39fa55b8e10209b2e7ed8
Tudor Avram <a96tudor@gmail.com>
2018-02-15 11:39:16 +0000

    a

---
+++ data/utils.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+
+def read_data_from_csv(file, dtype=np.ndarray, drop_cols=None):
+    """
+
+    :param file:            Path to the csv file where to read the data from
+    :param dtype:
+    :param
+    :return:
+    """
---
+++ models/pnn.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+class ProbabilisticNeuralNetwork:
+
+    def __init__(self, data):
+        """
+                CONSTRUCTOR
+
+        :param data:        Path to reading data
+        """
