35254b8a2b6a53bcbad609ae8fa27e88b727a9e5
Tudor Avram <a96tudor@gmail.com>
2018-02-15 12:20:20 +0000

    added data reading and df splitting functionalities

--- data/utils.py
+++ data/utils.py
@@ -2,11 +2,62 @@ import pandas as pd
 import numpy as np
 
 
-def read_data_from_csv(file, dtype=np.ndarray, drop_cols=None):
+def read_data_from_csv(file, label_cols, drop_cols=None):
     """
 
     :param file:            Path to the csv file where to read the data from
-    :param dtype:
-    :param
-    :return:
+    :param drop_cols:       List of columns to be dropped from the read data. Default None
+    :param label_cols:      List of columns that specify labels
+
+    :return:                The data, either as an np.ndarray or as a pd.DataFrame
+                            By 'the data', I mean a pair (X, Y) where X are the features and Y are the labels
     """
+
+    df = pd.read_csv(file)
+
+    if drop_cols is not None:
+        df = df.drop(drop_cols)
+
+    df_Ys = df[label_cols]
+
+    df_Xs = df[list(set(df.columns.values) - set(label_cols))]
+
+    return df_Xs, df_Ys
+
+
+def split_dataframe(df, label_cols, percentile=.75):
+    """
+
+    :param df:              the dataframe we want to split
+    :param percentile:      the % of the ndarray that will still be in the 1st part. Default .75
+    :return:                4 dataframes, representing:
+
+                                1. 1st part features
+                                2. 1st part labels
+                                3. 2nd part features
+                                4. 2nd part labels
+    """
+
+    rows_cnt = len(df)
+    rows_cnt_1st = int(rows_cnt * percentile)
+    rows_cnt_2nd = int(rows_cnt - rows_cnt_1st)
+
+    X_cols = list(set(df.columns.values) - set(label_cols))
+    Y_cols = label_cols
+
+    # GETTING RANDOM INDEXES FOR 1st part
+    idx_1st = np.random.choice(rows_cnt, size=rows_cnt_1st)
+    idx_2nd = list(set(range(rows_cnt)) - set(idx_1st))
+
+    df_1st_Xs = df.iloc[idx_1st, X_cols]
+    df_1st_Ys = df.iloc[idx_1st, Y_cols]
+    df_2nd_Xs = df.iloc[idx_2nd, X_cols]
+    df_2nd_Ys = df.iloc[idx_2nd, Y_cols]
+
+    return df_1st_Xs, df_1st_Ys, \
+           df_2nd_Xs, df_2nd_Ys
+
+
+
+
+
