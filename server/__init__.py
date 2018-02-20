6d224c1e8d6bf3671f21aa4e9bbcc5082c481356
Tudor Avram <a96tudor@gmail.com>
2018-02-20 20:17:14 +0000

    added skeleton for REST API

---
+++ server/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask, request, Response, jsonify
+
+def get_app(config_name):
+    """
+    :param config_name:     Type of configuration wanted for the REST API.
+                            Has to be one of: 'development', 'testing',
+                                              'staging', 'production'
+    :return:
+    """
+    return None
