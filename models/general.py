736b9e6e5c70b3364c319200b5dd4a9060916499
Tudor Avram <a96tudor@gmail.com>
2018-03-04 19:01:09 +0000

    updated data/utils

--- models/general.py
+++ models/general.py
@@ -6,3 +6,16 @@ UNWANTED_COLUMNS = [
 LABEL_COLS = [
     "SHOW", "HIDE"
 ]
+
+FEATURE_COLS = [
+    'NODE_TYPE',
+    'NEIGH_TYPE',
+    'EDGE_TYPE',
+    'WEB_CONN',
+    'NEIGH_WEB_CONN',
+    'UID_STS',
+    'GID_STS',
+    'VERSION',
+    'SUSPICIOUS',
+    'EXTERNAL'
+]
